dae978205b98bd515b8bacf642a80ab3b9602fa9
ppedvLukasK <lukask@ppedv.de>
2023-08-14 16:12:42 +0200

    Tag 1

--- AspNetClass.Web.Mvc/Controllers/CustomersController.cs
+++ AspNetClass.Web.Mvc/Controllers/CustomersController.cs
@@ -1,5 +1,7 @@
 ﻿using AspNetClass.DataAccess.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
 
 namespace AspNetClass.Web.Mvc.Controllers
 {
@@ -10,7 +12,6 @@ namespace AspNetClass.Web.Mvc.Controllers
         public CustomersController(NorthwindContext context)
         {
             _db = context;
-
         }
 
         [HttpGet]
@@ -25,6 +26,76 @@ namespace AspNetClass.Web.Mvc.Controllers
 		public IActionResult Create()
 		{
 			return View(nameof(Create));
+		}
+
+		[HttpPost]
+		public IActionResult Create(Customer customer)
+		{
+			if (ModelState.IsValid) //Prüfe, ob der State von EntityFramework gleich ist mit dem State der Entities die bereits herausgenommen wurden
+			{
+				if (!_db.Customers.Select(e => e.CustomerId).Contains(customer.CustomerId)) //Prüfe ob der Datensatz bereits existiert
+				{
+					_db.Customers.Add(customer);
+					_db.SaveChanges();
+				}
+			}
+
+			return View("Customers");
+		}
+
+		[HttpGet]
+		public IActionResult Edit(string id)
+		{
+			Customer cust = _db.Customers.FirstOrDefault(e => e.CustomerId == id);
+			if (cust != null)
+			{
+				return View(nameof(Edit), cust);
+			}
+			return NotFound(); //Für beliebige StatusCodes kann die Methode StatusCode(<Code>) verwendet werden
+		}
+
+		[HttpPost]
+		public IActionResult Edit(Customer cust)
+		{
+			if (!ModelState.IsValid)
+				return NotFound();
+
+			_db.Attach(cust).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+			_db.SaveChanges();
+			return View("Customers", _db.Customers);
+		}
+
+		[HttpGet]
+		public IActionResult Delete(string id)
+		{
+			Customer cust = _db.Customers.FirstOrDefault(e => e.CustomerId == id);
+			if (cust != null)
+			{
+				return View(nameof(Delete), cust);
+			}
+			return View("Customers", _db.Customers);
+		}
+
+		[HttpPost]
+		[ActionName("Delete")] //ActionName: Erlaubt umbenennen der Methode damit sie zur View passt
+		public IActionResult DeleteConfirm(string id)
+		{
+			Customer cust = _db.Customers.FirstOrDefault(e => e.CustomerId == id);
+			if (cust == null)
+				return NotFound();
+
+			_db.Customers.Remove(cust);
+			_db.SaveChanges();
+			return View("Customers", _db.Customers);
+		}
+
+		[HttpGet]
+		public IActionResult Details(string id)
+		{
+			Customer cust = _db.Customers.FirstOrDefault(e => e.CustomerId == id);
+			if (cust == null)
+				return NotFound();
+			return View("Details", cust);
 		}
 	}
 }
